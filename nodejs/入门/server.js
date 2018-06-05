b65e9fbc19f72a2cf4b50cd789f3365791fcb419
tiandashu <1074025714@qq.com>
2018-06-05 22:32:01 +0800

    dfdf

--- nodejs/入门/server.js
+++ nodejs/入门/server.js
@@ -2,13 +2,17 @@ var http = require("http");
 var url = require("url");
 
 var start = function(route,handle){
-    http.createServer(function(request,response){
-        var pathname = url.parse(request.url).pathname;
-        var content = route(handle,pathname);
-        
-        response.writeHead(200,{"content-Type":"text/plain"});
-        response.write(content);
-        response.end();
+    http.createServer(function(resquest,response){
+        var postData = "";
+        resquest.setEncoding("utf8");
+        var pathname = url.parse(resquest.url).pathname;
+        resquest.addListener("data",function(postDataChunk){
+            postData += postDataChunk;
+        })
+        resquest.addListener("end",function(){
+            route(handle,pathname,response,postData);
+        });
+       
     }).listen(8888);
 }
 
