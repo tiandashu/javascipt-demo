c078dd9396f91187d318bffadc537c1a7d23c4e4
tianlu <1074025714@qq.com>
2018-06-14 09:30:56 +0800

    dfdgf

--- nodejs/day7/03.网络操作/client.js
+++ nodejs/day7/03.网络操作/client.js
@@ -2,35 +2,37 @@
 var http = require('http');
 const querystring = require('querystring');
 const postData = querystring.stringify({
-    'msg' : 'Hello World!'
-  });
+    'key' : '18734233259'
+});
   
-  const options = {
+const options = {
     hostname: 'www.apengdai.com',
     port: 80,
-    path: '/encrypt/getAccessKey?key=18734233259',
+    path: '/encrypt/getAccessKey',
     method: 'POST',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData)
     }
-  };
+};
   
-  const req = http.request(options, (res) => {
+const req = http.request(options, (res) => {
     console.log(`状态码: ${res.statusCode}`);
     console.log(`响应头: ${JSON.stringify(res.headers)}`);
     res.setEncoding('utf8');
     res.on('data', (chunk) => {
-      console.log(`响应主体: ${chunk}`);
+        console.log(`响应主体: ${chunk}`);
     });
     res.on('end', () => {
-      console.log('响应中已无数据。');
+        console.log('响应中已无数据。');
     });
-  });
+});
   
-  req.on('error', (e) => {
+req.on('error', (e) => {
     console.error(`请求遇到问题: ${e.message}`);
-  });
+});
   
-  // 写入数据到请求主体
-  req.write(postData);
-  req.end();
+// 写入数据到请求主体
+console.log(postData);
+req.write(postData);
+req.end();
