f71bbf4a5b90672eac6cee1306c94c19f6e7414e
tiandashu <1074025714@qq.com>
2018-06-27 21:38:55 +0800

    dfg

--- nodejs/day7/示例/index4/lib/main.js
+++ nodejs/day7/示例/index4/lib/main.js
@@ -13,6 +13,9 @@ function spawn(server, config){
 }
 
 function main(argv){
+    //注意 ../lib/index.js路径的问题，这个路径和node执行时候的路径是相对的
+    //比如在inde4文件夹下执行 node lib/main.js conf/config.json    此处应改为 ../lib/index.js
+    //比如在inde4/bin文件夹下执行 node ../lib/main.js ../conf/config.json    此处应改为 ../lib/index.js
     spawn('../lib/index.js', argv[0]);
     console.log(process.pid, worker.pid);
     process.on('SIGTERM',function(){
